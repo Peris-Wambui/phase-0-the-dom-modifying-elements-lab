1512a795dfe92feadd71c84141542235b43b2e80
Peris-Wambui <wangechi.wambui@student.moringaschool.com>
2022-08-25 09:47:44 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+// will get the first element with the tag 
+// const newHeader = document.querySelector('h1');
+
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+newHeader.innerHTML = (' to include is the champion'); 
